02524ac90a5901964c1b323e31a74e76ff618d61
isabelalberdi <isabelalaberdi@gmail.com>
2018-03-12 09:51:12 -0300

    formato login

--- js/pfcheck.js
+++ js/pfcheck.js
@@ -1,5 +1,5 @@
 function pfcheck(){
-    let pfh = '7b3d3f67568ec003663ff63f9f310fe923cca43ee4c366e9fad030d40205f0f6';
+    let pfh = 'd01c0c96d980baa4213ad787baed2bdc7876d071deb1667bc8ce36f0e44a1716';
     var SHA256 = new Hashes.SHA256();
     var userinput = sessionStorage.getItem('pf');
     return pfh == SHA256.hex(userinput);
